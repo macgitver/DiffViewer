b6ad8d31aec71f7b45f3a3e4330fed508a5b89a7
Sascha Cunz <Sascha@BabbelBox.org>
2012-11-04 19:02:20 +0100

    View Color Settings

--- src/Main/MainWindow.cpp
+++ src/Main/MainWindow.cpp
@@ -1,5 +1,11 @@
 
+#include <QVBoxLayout>
+#include <QDialog>
+#include <QDialogButtonBox>
+
 #include "MainWindow.hpp"
+
+#include "libHeaven/ColorSchemata/ColorManager.hpp"
 
 MainWindow::MainWindow()
 {
@@ -10,10 +16,29 @@ void MainWindow::onFileOpen()
 {
 }
 
+void MainWindow::onSettingsColors()
+{
+    QDialog d;
+
+    QDialogButtonBox* box = new QDialogButtonBox;
+    box->setStandardButtons( QDialogButtonBox::Ok );
+    connect( box,SIGNAL(accepted()), &d, SLOT(accept()) );
+
+    QVBoxLayout* l = new QVBoxLayout;
+    l->addWidget( Heaven::ColorManager::self().createEditorWidget() );
+    l->addWidget( box );
+
+    d.setLayout( l );
+    d.setWindowTitle( tr( "Color Schema" ) );
+    d.exec();
+}
+
 void MainWindow::setupUi()
 {
     setupActions( this );
     setMenuBar( mbMain->menuBarFor( this ) );
 
     resize( 1000, 700 );
+
+//    Heaven::ColorManager::self().initialize();
 }
--- src/Main/MainWindow.hpp
+++ src/Main/MainWindow.hpp
@@ -14,6 +14,7 @@ public:
 
 private slots:
     void onFileOpen();
+    void onSettingsColors();
 
 private:
     void setupUi();
